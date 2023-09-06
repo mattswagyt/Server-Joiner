0e0ef5da35822f0ef3446b23ffdc82f2b75b1d0e
mattswagyt <128666675+mattswagyt@users.noreply.github.com>
2023-09-05 23:32:35 -0500

    Add files via upload

--- ServerJoiner.plugin.js
+++ ServerJoiner.plugin.js
@@ -1,10 +1,11 @@
-//META{"name":"ServerJoiner","version":"1.9.3","description":"Join up to 200 servers.","author":"BMF","website":"https://github.com/yourusername","source":"https://github.com/yourusername/ServerJoiner.plugin.js"}*//
+//META{"name":"ServerJoiner","version":"1.9.3","description":"Join up to 200 Servers without Nitro.","author":"BMF","website":"https://github.com/mattswagyt","source":"https://github.com/mattswagyt/Server-Joiner"}*//
 
 console.log("ServerJoiner plugin started.");
 
 class ServerJoiner {
   constructor() {
-    this.joinCount = 0;
+    this.joinQueue = [];
+    this.isJoining = false;
   }
 
   start() {
@@ -14,14 +15,24 @@ class ServerJoiner {
 
     // Listen for the context menu event on server icons
     document.addEventListener("contextmenu", this.handleContextMenu);
+    
+    BdApi.showToast("ServerJoiner plugin is active.", {
+      type: "success",
+      namespace: "server-joiner-plugin"
+    });
   }
 
   stop() {
     BdApi.clearCSS("server-joiner-css");
     document.removeEventListener("contextmenu", this.handleContextMenu);
+    
+    BdApi.showToast("ServerJoiner plugin is now inactive.", {
+      type: "error",
+      namespace: "server-joiner-plugin"
+    });
   }
 
-  handleContextMenu = (event) => {
+  handleContextMenu = async (event) => {
     const target = event.target;
 
     // Check if the right-clicked element is a server icon
@@ -29,16 +40,12 @@ class ServerJoiner {
       event.preventDefault();
 
       // Check if the user hasn't reached the join limit
-      if (this.joinCount < 200) {
-        this.joinServer(target);
+      if (this.joinQueue.length < 200) {
+        this.joinQueue.push(target);
+        this.processJoinQueue();
       } else {
-        // If attempting to join more than 100 servers, prevent the Nitro prompt
-        const joinButton = target.querySelector(".join-23GAYs");
-        if (joinButton) {
-          joinButton.click();
-        }
-
-        BdApi.showToast("You have reached the maximum join limit.", {
+        // If attempting to join more than 200 servers, show an error message
+        BdApi.showToast("You have reached the maximum join limit (200 servers).", {
           type: "error",
           namespace: "server-joiner-plugin"
         });
@@ -46,19 +53,38 @@ class ServerJoiner {
     }
   };
 
-  joinServer(target) {
+  processJoinQueue() {
+    if (this.isJoining || this.joinQueue.length === 0) {
+      return;
+    }
+
+    const target = this.joinQueue.shift();
+    if (!target) {
+      return;
+    }
+
+    this.isJoining = true;
+
     // Simulate clicking the "Join Server" button
     const joinButton = target.querySelector(".join-23GAYs");
     if (joinButton) {
       joinButton.click();
 
-      // Increment the join count
-      this.joinCount++;
+      // Wait for a brief moment to check for Nitro prompt
+      setTimeout(() => {
+        const nitroPrompt = document.querySelector(".joinNitroPrompt-3LaGhr");
+        if (nitroPrompt) {
+          nitroPrompt.remove();
+          BdApi.showToast("Nitro prompt removed.", {
+            type: "success",
+            namespace: "server-joiner-plugin"
+          });
+        }
 
-      BdApi.showToast(`Joined server ${this.joinCount} of 200.`, {
-        type: "success",
-        namespace: "server-joiner-plugin"
-      });
+        // Increment the join count
+        this.isJoining = false;
+        this.processJoinQueue();
+      }, 1000); // Adjust the delay as needed
     }
   }
 }
